26794eeebb8c6f7a694d15307cfaad212f0e3c28
Íthan P. Amaral <ithanamaral@gmail.com>
2025-03-25 11:02:16 -0300

    melhora Proxy

--- src/Trabalho_ProgII/Proxy.java
+++ src/Trabalho_ProgII/Proxy.java
@@ -1,39 +1,39 @@
 package Trabalho_ProgII;
 
-// Interface que define o comportamento do controle de ingredientes
+
 interface IngredientesControle {
     void adicionar(String nome);
 }
 
-// Classe real que adiciona ingredientes
+
 class IngredienteAdicionado implements IngredientesControle {
     public void adicionar(String nome) {
         System.out.println("Adicionando: " + nome);
     }
 }
 
-// Proxy que bloqueia ingredientes proibidos
+
 class IngredientesPermitidos implements IngredientesControle {
     private IngredienteAdicionado ingredienteAdicionado = new IngredienteAdicionado();
-    private static final String INGREDIENTE_PROIBIDO = "Glúten";
+    private static final String ingrediente_proibido = "Açúcar";
 
     public void adicionar(String nome) {
-        if (nome.equalsIgnoreCase(INGREDIENTE_PROIBIDO)) {
-            System.out.println("⛔ ERRO: Ingrediente proibido! " + nome + " não pode ser adicionado.");
-        } else {
-            ingredienteAdicionado.adicionar(nome);
-        }
+        if(nome.equals(ingrediente_proibido)){
+            System.out.println("⛔ ERRO: Ingrediente proibido! \" + nome + \" não pode ser adicionado.");}
+        else{
+        ingredienteAdicionado.adicionar(nome);}
+
     }
 }
 
-// Classe principal com a execução do proxy
+
 public class Proxy{
     public static void main(String[] args) {
-        IngredientesControle controle = new IngredientesPermitidos();
+        IngredientesPermitidos controle = new IngredientesPermitidos();
 
-        controle.adicionar("Chocolate");
-        controle.adicionar("Farinha");
-        controle.adicionar("Glúten"); // Esse deve ser bloqueado
-        controle.adicionar("Ovos");
+        controle.adicionar("Chocolate Diet");
+        controle.adicionar("Maizena");
+        controle.adicionar("Açúcar"); // Esse deve ser bloqueado
+        controle.adicionar("Leite");
     }
 }
